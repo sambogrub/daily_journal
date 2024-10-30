1c7ac0a8a62169952b540884dfd6eee86e4f93ab
sambogrub <137734917+sambogrub@users.noreply.github.com>
2024-10-30 08:43:16 -0500

    Initial commit

---
+++ daily_journal/main.py
@@ -0,0 +1,11 @@
+'''Main module for daily journal app. Will initialize the controller,
+passing it both the repository and the ui'''
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
